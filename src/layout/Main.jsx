b250254142df583970987c0e7df724f7bacb9944
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-29 23:50:00 +0300

    Add omdbapi API

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,32 @@
-const Main = () => {
-  return ( 
-    <main className="container content">
-      Hello, Movie World
-    </main>
-  );
+import React, { Component } from 'react';
+import Movies from '../components/Movies';
+
+const API_KEY = '';
+
+class Main extends Component {
+  state = {
+    movies: [ ],
+  }
+
+  componentDidMount() {
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+      .then(response => response.json())
+      .then(data => this.setState({ movies: data.Search }))
+  }
+
+  render() {
+    const {movies} = this.state;
+
+    return ( 
+      <main className="container content">
+        {movies.length 
+          ? (<Movies movies={this.state.movies} />)
+          : <h3>Loading...</h3> 
+        }
+        
+      </main>
+    );
+  }
 }
 
 export default Main;
