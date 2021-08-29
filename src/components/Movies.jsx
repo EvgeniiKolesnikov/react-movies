81aaac8229d7122eeca991035b83167cb37b11ce
Evgenii Kolesnikov <jekow@yandex.ru>
2021-08-30 01:42:06 +0300

    Add filter movies and .env.local REACT_APP_API_KEY

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,15 +1,17 @@
-import Movie from "./Movie";
+import Movie from './Movie';
 
 const Movies = (props) => {
-  const {movies} = props;
+  const { movies = [] } = props;
 
-  return ( 
-    <div className="movies">
-      {movies.map((movie) => (
-        <Movie key={movie.imdbID} {...movie} />
-      ))}
+  return (
+    <div className='movies'>
+      {movies.length ? (
+        movies.map((movie) => <Movie key={movie.imdbID} {...movie} />)
+      ) : (
+        <h4>Nothing found</h4>
+      )}
     </div>
   );
-}
+};
 
 export default Movies;
